b1892af6d1df89116dc31a4796c6d275fa9a347c
Alain Pham <apham@work>
2025-03-16 18:24:36 +0100

    slock 64 bits only

--- config.mk
+++ config.mk
@@ -28,7 +28,7 @@ IMLIB=-lImlib2
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr ${XINERAMA} ${PAM} ${IMLIB}
+LIBS = -L/usr/lib64 -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr ${XINERAMA} ${PAM} ${IMLIB}
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H ${XINERAMAFLAGS} ${BSD} ${NETBSD}
